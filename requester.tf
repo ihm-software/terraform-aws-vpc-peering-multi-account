ceba8df794f127359c287700e9b66898fdc00cf2
brunov <brunov@ciandt.com>
2023-10-05 12:10:47 -0300

    [FALCON9-1038] First filter test

--- requester.tf
+++ requester.tf
@@ -159,7 +159,11 @@ resource "aws_vpc_peering_connection_options" "requester" {
 locals {
   requester_aws_route_table_ids           = try(distinct(sort(data.aws_route_table.requester.*.route_table_id)), [])
   requester_aws_route_table_ids_count     = length(local.requester_aws_route_table_ids)
-  requester_cidr_block_associations       = flatten(data.aws_vpc.requester.*.cidr_block_associations)
+  # requester_cidr_block_associations       = flatten(data.aws_vpc.requester.*.cidr_block_associations)
+  requester_cidr_block_associations       = flatten(tolist(setsubtract([ 
+    for k,v in data.aws_vpc.requester.*.cidr_block_associations : v.cidr_block
+    #], var.requester_ignore_cidrs)))  
+    ], "100.64.0.0/16")))  
   requester_cidr_block_associations_count = length(local.requester_cidr_block_associations)
 }
 
